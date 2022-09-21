adbcd27d80e70521689af7c4ba94db5b02212ff0
eeshaprakarsh <prakarsheesha@gmail.com>
2022-09-21 08:07:34 +0530

    Table view added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import Form  from './components/Form';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-
-  
   return (
     <div className="App">
-    <Form></Form>
+      <Form></Form>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,36 @@
-import { useState } from 'react';
-import Papa from 'papaparse';
-
+import { useState } from "react";
+import Papa from "papaparse";
 
 const Form = () => {
-
   const [header, setHeader] = useState("Header");
- // State to store parsed data
+  // State to store parsed data
   const [parsedData, setParsedData] = useState([]);
 
   //State to store table Column name
   const [tableRows, setTableRows] = useState([]);
 
   //State to store the values
- const [values, setValues] = useState([]);
- 
- const changeHandler = (event) => {
-  
-if (event&&event.target) setHeader(event.target.files[0].name);
-  
+  const [values, setValues] = useState([]);
+
+  function validURL(str) {
+    var pattern = new RegExp(
+      "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+      "i"
+    ); // fragment locator
+
+    return !!pattern.test(str);
+  }
+
+  const changeHandler = (event) => {
+    if (event && event.target) setHeader(event.target.files[0].name);
+
     // Passing file data (event.target.files[0]) to parse using Papa.parse
-   Papa.parse(event.target.files[0], {
-      
+    Papa.parse(event.target.files[0], {
       header: true,
       skipEmptyLines: true,
       complete: function (results) {
@@ -43,51 +53,77 @@ if (event&&event.target) setHeader(event.target.files[0].name);
         setValues(valuesArray);
       },
     });
-  
-  
   };
- 
-    return (
+
+  return (
+    <div>
+      <h2>{header} </h2>
       <div>
-
-
-            <h2>{header} </h2>
-             <div>
-      {/* File Uploader */}
-      <input
-        type="file"
-        name="file"
-        onChange={changeHandler}
-        accept=".csv"
-        style={{ display: "block", margin: "10px auto" }}
-      />
-      <br />
-      <br />
-      {/* Table */}
-      <table>
-        <thead>
-          <tr>
-            {tableRows.map((rows, index) => {
-              return <th key={index}>{rows}</th>;
+        {/* File Uploader */}
+        <input
+          type="file"
+          name="file"
+          onChange={changeHandler}
+          accept=".csv"
+          style={{ display: "block", margin: "10px auto" }}
+        />
+        <br />
+        <br />
+        {/* Table */}
+        <table>
+          <thead>
+            <tr>
+              {tableRows.map((rows, index) => {
+                return (
+                  <th
+                    key={index}
+                    style={{
+                      padding: "10px 20px",
+                      backgroundColor: "#2E8BC0",
+                      width: "50px",
+                      color: "#fff",
+                    }}
+                  >
+                    {rows}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {values.map((value, index) => {
+              return (
+                <tr key={index}>
+                  {value.map((val, i) => {
+                    let imgId = val.split("id=");
+                    if (i === 1) {
+                      // checkImage(val)
+                      return (
+                        <td key={i} style={{ border: "1px solid #D3D3D3" }}>
+                          <img
+                            referrerPolicy="no-referrer"
+                            src={`https://drive.google.com/uc?id=${imgId[1]}`}
+                            width="50%"
+                            height="50%"
+                          />
+                        </td>
+                      );
+                    } else {
+                      return (
+                        <td key={i} style={{ border: "1px solid #D3D3D3" }}>
+                          {val}
+                        </td>
+                      );
+                    }
+                  })}
+                </tr>
+              );
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {values.map((value, index) => {
-            return (
-              <tr key={index}>
-                {value.map((val, i) => {
-                  return <td key={i}>{val}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </div>
-
-        </div>
-    );
-}
+  );
+};
 
 export default Form;
